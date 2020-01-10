be05c85f394aad5b83920383f6fe46e75a851b94
Carlos <Carlos@DESKTOP-Q51GSC7>
2020-01-10 12:07:20 +0100

    GeneradorBean crea los bean maria y juan

--- InversionOfControl/src/main/java/com/carlos/IoC/beans/GeneradorAutores.java
+++ InversionOfControl/src/main/java/com/carlos/IoC/beans/GeneradorAutores.java
@@ -1,6 +1,31 @@
 package com.carlos.IoC.beans;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.carlos.IoC.beans.Autor.AutorBean;
+
+@Configuration
 public class GeneradorAutores {
 
+	@Bean
+	public AutorBean juan() {
+		
+		AutorBean autor = new AutorBean();
+		autor.setNombre("Juan");
+		autor.setEdad(22);
+		
+		return autor;
+	}
+	
+	@Bean
+	public AutorBean maria() {
+		
+		AutorBean autor = new AutorBean();
+		autor.setNombre("Maria");
+		autor.setEdad(20);
+		
+		return autor;
+	}
 	
 }
